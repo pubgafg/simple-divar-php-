53e7c81fdc526bfabc86497b008bd194177f2aa0
pubgafg <pubgafg82@gmail.com>
2025-09-10 20:22:20 +0430

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'divar_simple');
+define('DB_USER', 'root');   // نام کاربری دیتابیس
+define('DB_PASS', '');       // رمز دیتابیس
+
+define('UPLOAD_DIR', __DIR__ . '/uploads/');
+define('UPLOAD_URI', '/uploads/');
