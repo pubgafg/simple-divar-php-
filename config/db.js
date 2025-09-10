d23af9ea95e7c2c024f0a517d16352fa0980c851
pubgafg <pubgafg82@gmail.com>
2025-09-10 21:54:50 +0430

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/simple-divar', {
+    const mongoURI = process.env.MONGO_URI || 
+      'mongodb+srv://simple-divar:Afghan123@cluster0.mongodb.net/simple-divar?retryWrites=true&w=majority';
+
+    await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
+
     console.log('MongoDB Connected');
   } catch (err) {
-    console.error(err.message);
+    console.error('MongoDB connection error:', err.message);
     process.exit(1);
   }
 };
