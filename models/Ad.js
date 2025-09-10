616dcb351bb49837c803f11ff9a9bc24293db9b7
pubgafg <pubgafg82@gmail.com>
2025-09-10 21:15:23 +0430

    Update Ad.js

--- models/Ad.js
+++ models/Ad.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const AdSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Ad', AdSchema);
