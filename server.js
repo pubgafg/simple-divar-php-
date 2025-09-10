d093132472ad21a9447dee1f2671db688935b862
pubgafg <pubgafg82@gmail.com>
2025-09-10 21:15:56 +0430

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const adsRoutes = require('./routes/ads');
+const path = require('path');
 
+connectDB();
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/ads', adsRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
